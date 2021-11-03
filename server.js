e99fdffd6669a6a1e95ad01e8ce49305c113c170
Matheus Torsoni de Campos <91149036+matheustorsoni@users.noreply.github.com>
2021-11-03 15:09:52 -0300

    bug fix

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var http = require("http");
+const http = require("http");
+const port = 3000;
+const stats = require("./pcRamUsage.js");
 
-http.createServer(function (request, response) {
-
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-
-   response.end('Hello World\n');
-}).listen(8000);
-
-console.log('Server running at http://127.0.0.1:8000/')
+http
+  .createServer((req, res) => {
+    var url = req.url;
+    if (url === "/stats") {//write a response
+      res.end(JSON.stringify(stats, null, 2)); //end the response
+    } else {
+      res.write("<h1>Working</h1>"); //write a response
+      res.end();
+    }
+  })
+  .listen(port, () =>
+    console.log(`Server is running in http://localhost:${port}`)
+  );
